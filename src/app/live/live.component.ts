c2dacfa3699c163f3b74efd1ade997bbf4139055
achrefabdennebi <achref.abdennebi@gmail.com>
2024-12-31 18:54:22 +0100

    feat: improve the live component view

--- src/app/live/live.component.ts
+++ src/app/live/live.component.ts
@@ -1,23 +1,27 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
-import { map, startWith, switchMap } from 'rxjs';
+import { catchError, map, of, startWith, switchMap } from 'rxjs';
 import { RaceService } from '../race.service';
 import { RaceModel } from '../models/race.model';
 import { PonyWithPositionModel } from '../models/pony.model';
 import { PonyComponent } from '../pony/pony.component';
+import { FromNowPipe } from '../from-now.pipe';
 
 interface RaceModelWithPositions extends RaceModel {
   poniesWithPosition: Array<PonyWithPositionModel>;
+  liveError?: boolean;
 }
 
 @Component({
-  imports: [PonyComponent],
+  imports: [PonyComponent, FromNowPipe],
   templateUrl: './live.component.html',
   styleUrl: './live.component.css'
 })
 export class LiveComponent {
   readonly raceModel: Signal<RaceModelWithPositions | undefined>;
+  readonly winners = computed(() => this.raceModel()?.poniesWithPosition.filter(pony => pony.position >= 100));
+  readonly betWon = computed(() => this.winners()?.some(pony => pony.id === this.raceModel()?.betPonyId));
 
   constructor() {
     const route = inject(ActivatedRoute);
@@ -25,12 +29,17 @@ export class LiveComponent {
     const raceService = inject(RaceService);
     this.raceModel = toSignal(
       raceService.get(raceId).pipe(
-        switchMap(race =>
-          raceService.live(race!.id).pipe(
-            map(live => ({ ...race, poniesWithPosition: live.ponies, status: live.status })),
-            startWith({ ...race, poniesWithPosition: [] })
-          )
-        )
+        switchMap(race => {
+          if (race.status === 'FINISHED') {
+            return of({ ...race, poniesWithPosition: [] });
+          } else {
+            return raceService.live(race.id).pipe(
+              map(live => ({ ...race, poniesWithPosition: live.ponies, status: live.status })),
+              startWith({ ...race, poniesWithPosition: [] }),
+              catchError(() => of({ ...race, poniesWithPosition: [], liveError: true }))
+            );
+          }
+        })
       )
     );
   }
